ed8e1f57dc66335be6fb6587aba1e00543ff01e6
Wu, Jiantao <jiantao.wu@alumni.ucl.ac.uk>
2020-09-27 06:36:38 +0000

    Introduction to the implementation of our project

--- README.md
+++ README.md
@@ -1,11 +1,26 @@
 # A Ontology Model for Climate Data Analysis
 This repository includes a complete collection of codes required to generate the results and diagrams presented in the paper: 
 > published details of the paper
-## Folder Description
-Organization of the code
-- demos/paper_ca_ontology    The root path of the project
-  - csv2rdf_py/
-    - datasets/                Raw climate datasets of Shanghai and Dublin downloaded from [NOAA](https://www.ncdc.noaa.gov/cdo-web/)
-    - ca_model.py              The script to produce Climate Analysis Ontology
+## Folder Structure
+paper_ca_ontology
+├── csv2rdf_py
+│   ├── ca_model.py
+│   └── datasets
+│       ├── dublinfull.csv
+│       └── shanghaifull.csv
+└── dataplot
+    ├── globalwarming_sh.png
+    ├── globalwarming_sh.py
+    ├── p_dublin_1_12.csv
+    ├── p_shanghai_1_12.csv
+    ├── plot_P_sh_dbl.py
+    ├── plot_T_sh_dbl.py
+    ├── preciptitation.png
+    ├── t_dublin_1_12.csv
+    ├── t_shanghai_1_12.csv
+    └── temperature.png
 
+`paper_ca_ontology/` Root directory, `csv2rdf_py\` includes the `ca_model.py` file which uses the raw csv data downloaded from [NOAA](https://www.ncdc.noaa.gov/cdo-web/) then stored in `datasets\` to generate the triples in line with CA ontology.
+
+`dataplot\` contains a group of scripts `*.py` that make use of a group of `*.csv` files which are the csv form of the solutions to the queries executed in our Fuseki endpoint to result the pictures `*.png` presented in our paper. Note: 'p' or 'P' in file's name represents 'precipitation' and 't' or 'T' means 'temperature'.
  
--- demos/paper_ca_ontology/csv2rdf_py/ca_model.py
+++ demos/paper_ca_ontology/csv2rdf_py/ca_model.py
@@ -11,22 +11,22 @@ wgs84 = Namespace("http://example.org/people/")
 qudt = Namespace("http://qudt.org/1.1/schema/qudt#")
 unit = Namespace("http://qudt.org/1.1/vocab/unit#")
 dul = Namespace("http://www.loa-cnr.it/ontologies/DUL.owl#")
-eca = Namespace("http://example.org/eca/ont/")
+ca = Namespace("http://example.org/ca/ont/")
 cf =  Namespace("http://purl.oclc.org/NET/ssnx/cf/cf-feature")
 
 
-eca_str = "http://example.org/eca/ont/"
+ca_str = "http://example.org/ca/ont/"
 
-eca_tprtSensor = URIRef(eca_str + "Class/TemperatureSensor")
-eca_prcpSensor = URIRef(eca_str + "Class/PrecipitationSensor")
-eca_tprtObservation = URIRef(eca_str + "Class/TemperatureObservation")
-eca_prcpObservation = URIRef(eca_str + "Class/PrecipitationObservation")
-eca_tprtResult = URIRef(eca_str + "Class/TemperatureResult")
-eca_prcpResult = URIRef(eca_str + "Class/PrecipitationResult")
+ca_tprtSensor = URIRef(ca_str + "Class/TemperatureSensor")
+ca_prcpSensor = URIRef(ca_str + "Class/PrecipitationSensor")
+ca_tprtObservation = URIRef(ca_str + "Class/TemperatureObservation")
+ca_prcpObservation = URIRef(ca_str + "Class/PrecipitationObservation")
+ca_tprtResult = URIRef(ca_str + "Class/TemperatureResult")
+ca_prcpResult = URIRef(ca_str + "Class/PrecipitationResult")
 
-triples_classes = [(eca_tprtSensor, RDFS.subClassOf, SOSA.Sensor), (eca_prcpSensor, RDFS.subClassOf, SOSA.Sensor), 
-(eca_tprtObservation, RDFS.subClassOf, SOSA.Observation), (eca_prcpObservation, RDFS.subClassOf, SOSA.Observation), 
-(eca_tprtResult, RDFS.subClassOf, SOSA.Result), (eca_prcpResult, RDFS.subClassOf, SOSA.Result)]
+triples_classes = [(ca_tprtSensor, RDFS.subClassOf, SOSA.Sensor), (ca_prcpSensor, RDFS.subClassOf, SOSA.Sensor), 
+(ca_tprtObservation, RDFS.subClassOf, SOSA.Observation), (ca_prcpObservation, RDFS.subClassOf, SOSA.Observation), 
+(ca_tprtResult, RDFS.subClassOf, SOSA.Result), (ca_prcpResult, RDFS.subClassOf, SOSA.Result)]
 
 
 
@@ -44,49 +44,49 @@ numof_rowitems = len(record_head)
 triples_lst = list()
 
 for row in record_rows:
-    iri_station =  URIRef(eca_str + row[0])
-    iri_station_location = URIRef(eca_str + row[0] + "/" + "location")
-    iri_station_sensor_prcp =  URIRef(eca_str + "sensor/" + row[0] + "/" + "prcp")
-    iri_station_sensor_tprt =  URIRef(eca_str + "sensor/" + row[0] + "/" + "tprt")
-    bn_station_sensor_prcp_observation = URIRef(eca_str + "obsv/" + row[0] + "/"+ "sensor/" + "prcp/" + row[5])
-    bn_station_sensor_tprt_observation = URIRef(eca_str + "obsv/" + row[0] + "/"+ "sensor/" + "tprt/" + row[5])
+    iri_station =  URIRef(ca_str + row[0])
+    iri_station_location = URIRef(ca_str + row[0] + "/" + "location")
+    iri_station_sensor_prcp =  URIRef(ca_str + "sensor/" + row[0] + "/" + "prcp")
+    iri_station_sensor_tprt =  URIRef(ca_str + "sensor/" + row[0] + "/" + "tprt")
+    bn_station_sensor_prcp_observation = URIRef(ca_str + "obsv/" + row[0] + "/"+ "sensor/" + "prcp/" + row[5])
+    bn_station_sensor_tprt_observation = URIRef(ca_str + "obsv/" + row[0] + "/"+ "sensor/" + "tprt/" + row[5])
 
-    bn_prcp_result = URIRef(eca_str + row[0] + "/" + "result/" + "sensor/" + "prcp/" + row[5]) #BNode() 
-    bn_tprt_result_avg = URIRef(eca_str + row[0] + "/" + "result/" + "sensor/" + "tprt/" + "avg/" + row[5]) #BNode()
-    bn_tprt_result_max = URIRef(eca_str + row[0] + "/" + "result/" + "sensor/" + "tprt/" + "max/" + row[5]) #BNode()
-    bn_tprt_result_min = URIRef(eca_str + row[0] + "/" + "result/" + "sensor/" + "tprt/" + "min/" + row[5]) #BNode()
+    bn_prcp_result = URIRef(ca_str + row[0] + "/" + "result/" + "sensor/" + "prcp/" + row[5]) #BNode() 
+    bn_tprt_result_avg = URIRef(ca_str + row[0] + "/" + "result/" + "sensor/" + "tprt/" + "avg/" + row[5]) #BNode()
+    bn_tprt_result_max = URIRef(ca_str + row[0] + "/" + "result/" + "sensor/" + "tprt/" + "max/" + row[5]) #BNode()
+    bn_tprt_result_min = URIRef(ca_str + row[0] + "/" + "result/" + "sensor/" + "tprt/" + "min/" + row[5]) #BNode()
 
     triples_station = [(iri_station, RDF.type, SOSA.Platform),(iri_station, RDFS.label, Literal(row[1])),(iri_station, dul.hasLocation, iri_station_location)]
 
-    triples_station_location = [(iri_station_location, eca.locationID, Literal(row[0])),(iri_station_location, wgs84.lat, Literal(row[2])),(iri_station_location, wgs84.lon, Literal(row[3])),(iri_station_location, wgs84.alt, Literal(row[4]))]
+    triples_station_location = [(iri_station_location, ca.locationID, Literal(row[0])),(iri_station_location, wgs84.lat, Literal(row[2])),(iri_station_location, wgs84.lon, Literal(row[3])),(iri_station_location, wgs84.alt, Literal(row[4]))]
     
-    triples_sensor = [(iri_station_sensor_prcp, RDF.type, eca_prcpSensor),(iri_station_sensor_prcp, SOSA.isHostedby, iri_station),
+    triples_sensor = [(iri_station_sensor_prcp, RDF.type, ca_prcpSensor),(iri_station_sensor_prcp, SOSA.isHostedby, iri_station),
     (iri_station_sensor_prcp, SOSA.observes, cf.precipitation_amount)]
     
-    triples_sensor.extend([(iri_station_sensor_tprt, RDF.type, eca_tprtSensor),(iri_station_sensor_tprt, SOSA.isHostedby, iri_station),
+    triples_sensor.extend([(iri_station_sensor_tprt, RDF.type, ca_tprtSensor),(iri_station_sensor_tprt, SOSA.isHostedby, iri_station),
     (iri_station_sensor_tprt, SOSA.observes, cf.air_temperature)])
 
     triples_result = []
     
-    triples_abservation = [(bn_station_sensor_prcp_observation, RDF.type, eca_prcpObservation),(bn_station_sensor_prcp_observation, SOSA.resultTime,Literal(row[5],datatype=XSD.date)),
+    triples_abservation = [(bn_station_sensor_prcp_observation, RDF.type, ca_prcpObservation),(bn_station_sensor_prcp_observation, SOSA.resultTime,Literal(row[5],datatype=XSD.date)),
     (bn_station_sensor_prcp_observation, SOSA.observedProperty, cf.precipitation_amount),(bn_station_sensor_prcp_observation, SOSA.madeBySensor, iri_station_sensor_prcp)]
 
-    triples_abservation.extend([(bn_station_sensor_tprt_observation, RDF.type, eca_tprtObservation), (bn_station_sensor_tprt_observation, SOSA.resultTime, Literal(row[5],datatype=XSD.date)),
+    triples_abservation.extend([(bn_station_sensor_tprt_observation, RDF.type, ca_tprtObservation), (bn_station_sensor_tprt_observation, SOSA.resultTime, Literal(row[5],datatype=XSD.date)),
     (bn_station_sensor_tprt_observation, SOSA.observedProperty, cf.air_temperature),(bn_station_sensor_tprt_observation, SOSA.madeBySensor, iri_station_sensor_tprt)])
 
     if row[record_head.index("PRCP")] != "" :
         triples_abservation.extend([(bn_station_sensor_prcp_observation, SOSA.hasResult, bn_prcp_result)])
-        triples_result.extend([(bn_prcp_result, RDF.type, eca_prcpResult),(bn_prcp_result, RDF.type, qudt.QuantityValue),(bn_prcp_result, qudt.unit, unit.Inch),(bn_prcp_result, qudt.numericValue, Literal(row[record_head.index("PRCP")],datatype=XSD.float))])
+        triples_result.extend([(bn_prcp_result, RDF.type, ca_prcpResult),(bn_prcp_result, RDF.type, qudt.QuantityValue),(bn_prcp_result, qudt.unit, unit.Inch),(bn_prcp_result, qudt.numericValue, Literal(row[record_head.index("PRCP")],datatype=XSD.float))])
 
     if row[record_head.index("TAVG")] != "" :
         triples_abservation.extend([(bn_station_sensor_tprt_observation, SOSA.hasResult, bn_tprt_result_avg)])
-        triples_result.extend([(bn_tprt_result_avg, RDFS.label, Literal("avg")), (bn_tprt_result_avg, RDF.type, eca_tprtResult), (bn_tprt_result_avg, RDF.type, qudt.QuantityValue), (bn_tprt_result_avg, qudt.unit, unit.DegreeFahrenheit),(bn_tprt_result_avg, qudt.numericValue, Literal(row[record_head.index("TAVG")],datatype=XSD.float))])
+        triples_result.extend([(bn_tprt_result_avg, RDFS.label, Literal("avg")), (bn_tprt_result_avg, RDF.type, ca_tprtResult), (bn_tprt_result_avg, RDF.type, qudt.QuantityValue), (bn_tprt_result_avg, qudt.unit, unit.DegreeFahrenheit),(bn_tprt_result_avg, qudt.numericValue, Literal(row[record_head.index("TAVG")],datatype=XSD.float))])
     if row[record_head.index("TMIN")] != "" :
         triples_abservation.extend([(bn_station_sensor_tprt_observation, SOSA.hasResult, bn_tprt_result_min)])
-        triples_result.extend([(bn_tprt_result_min, RDFS.label, Literal("min")), (bn_tprt_result_min, RDF.type, eca_tprtResult), (bn_tprt_result_min, RDF.type, qudt.QuantityValue), (bn_tprt_result_min, qudt.unit, unit.DegreeFahrenheit),(bn_tprt_result_min, qudt.numericValue, Literal(row[record_head.index("TMIN")],datatype=XSD.float))])
+        triples_result.extend([(bn_tprt_result_min, RDFS.label, Literal("min")), (bn_tprt_result_min, RDF.type, ca_tprtResult), (bn_tprt_result_min, RDF.type, qudt.QuantityValue), (bn_tprt_result_min, qudt.unit, unit.DegreeFahrenheit),(bn_tprt_result_min, qudt.numericValue, Literal(row[record_head.index("TMIN")],datatype=XSD.float))])
     if row[record_head.index("TMAX")] != "" :
         triples_abservation.extend([(bn_station_sensor_tprt_observation, SOSA.hasResult, bn_tprt_result_max)])
-        triples_result.extend([(bn_tprt_result_max, RDFS.label, Literal("max")), (bn_tprt_result_max, RDF.type, eca_tprtResult), (bn_tprt_result_max, RDF.type, qudt.QuantityValue), (bn_tprt_result_max, qudt.unit, unit.DegreeFahrenheit),(bn_tprt_result_max, qudt.numericValue, Literal(row[record_head.index("TMAX")],datatype=XSD.float))])
+        triples_result.extend([(bn_tprt_result_max, RDFS.label, Literal("max")), (bn_tprt_result_max, RDF.type, ca_tprtResult), (bn_tprt_result_max, RDF.type, qudt.QuantityValue), (bn_tprt_result_max, qudt.unit, unit.DegreeFahrenheit),(bn_tprt_result_max, qudt.numericValue, Literal(row[record_head.index("TMAX")],datatype=XSD.float))])
 
     triples_temp = triples_station+triples_station_location + triples_sensor + triples_abservation + triples_result
 
@@ -99,7 +99,7 @@ g.bind("qudt", qudt)
 g.bind("dul", dul)
 g.bind("cf", cf)
 g.bind("unit",unit)
-g.bind("eca", eca)
+g.bind("ca", ca)
 g.bind("ssn", SSN)
 g.bind("sosa", SOSA)
 g.bind("rdfs", RDFS)
