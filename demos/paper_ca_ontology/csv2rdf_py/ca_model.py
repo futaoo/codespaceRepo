0fe0dbad1096e3a1ee6bd399cecc9fa861384ddc
Wu, Jiantao <jiantao.wu@alumni.ucl.ac.uk>
2020-10-07 05:59:59 +0000

    79-84

--- demos/paper_ca_ontology/csv2rdf_py/ca_model.py
+++ demos/paper_ca_ontology/csv2rdf_py/ca_model.py
@@ -31,7 +31,7 @@ triples_classes = [(ca_tprtSensor, RDFS.subClassOf, SOSA.Sensor), (ca_prcpSensor
 
 
 
-csvpath = './datasets/73_78.csv'
+csvpath = './datasets/15_20.csv'
 with open(csvpath) as f:
     csvreader =  csv.reader(f)
     record_head = list(next(csvreader))
